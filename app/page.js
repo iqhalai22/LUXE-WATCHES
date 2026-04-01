8cb6561dfcfcdb287867735a815125f818cdfce9
iqhalai22 <iqhalai@gmail.com>
2026-04-02 02:31:51 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,1 +1,28 @@
 app/page.js
+export default function Home() {
+  return (
+    <div style={{padding: "20px", fontFamily: "Arial"}}>
+      <h1>Luxe Watches ⌚</h1>
+      <p>Premium Watches at Affordable Prices</p>
+
+      <img 
+        src="https://via.placeholder.com/300"
+        style={{width: "100%", borderRadius: "10px"}}
+      />
+
+      <br /><br />
+
+      <a href="/order/1">
+        <button style={{
+          padding: "10px 20px",
+          backgroundColor: "black",
+          color: "white",
+          border: "none",
+          borderRadius: "5px"
+        }}>
+          Order Now
+        </button>
+      </a>
+    </div>
+  );
+}
